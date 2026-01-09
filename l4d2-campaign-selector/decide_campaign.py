275e1c16a76e61cb4e79e5abccaf2ef5451a20ac
Final-Eclipse <AnimeAndManga1@proton.me>
2026-01-09 13:18:32 -0500

    Update decide_campaign.py

--- l4d2-campaign-selector/decide_campaign.py
+++ l4d2-campaign-selector/decide_campaign.py
@@ -6,3 +6,14 @@ with open("left_4_dead_2_scraper/l4d2_mods.json", "r") as file:
 
 for mod_title in mod_dict:
     print(mod_title)
+
+import json
+
+with open("left_4_dead_2_scraper/l4d2_mods.json", "r") as file:
+    file_contents = file.read()
+    mods = json.loads(file_contents)
+
+for mod_title, mod_details in mods.items():
+    print(mod_title, mod_details)
+    print()
+    break
